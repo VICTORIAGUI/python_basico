57d4806810aac972f8045f0679ec922bb3d4b0ab
VICTORIAGUI <victoriagui.santos@gmail.com>
2022-12-05 19:23:01 -0300

    add funcoes

---
+++ tarefa-funcoes.py
@@ -0,0 +1,10 @@
+# Escreva uma função que recebe um número n como parametro e imprime se n é positivo ou negativo 
+
+def imprime_sinal(a):
+    if a <0:
+        print('Negativo')
+    else:
+            ('Positivo')
+
+imprime_sinal(-5)
+imprime_sinal(10)            
